09de092d40daf650f6bc66273dbd476c861621ed
Taylor Christensen <tchris43@byu.edu>
2026-01-23 13:26:14 -0700

    fixed moveDiagonally to correctly move to edge or end when blocked

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -25,14 +25,15 @@ public class ChessMove {
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        throw new RuntimeException("Not implemented");
+        return start;
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-        throw new RuntimeException("Not implemented");
+
+        return end;
     }
 
     /**
@@ -42,7 +43,7 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-        throw new RuntimeException("Not implemented");
+        return promotion;
     }
 
     @Override
--- shared/src/main/java/chess/MoveCalculator.java
+++ shared/src/main/java/chess/MoveCalculator.java
@@ -22,56 +22,69 @@ public class MoveCalculator {
         int currX = X;
         int currY = Y;
         while (currX > 1){
-            addMoves(currX-1, Y);
             currX = currX-1;
         }
+        addMoves(currX-1, Y);
         currX = X;
         while (currX < 8){
-            addMoves(currX + 1, Y);
             currX = currX + 1;
         }
+        addMoves(currX + 1, Y);
         while (currY > 1){
-            addMoves(currY-1, X);
             currY = currY - 1;
         }
+        addMoves(currY-1, X);
         currY = Y;
         while (currY < 8){
-            addMoves(currY + 1, X);
             currY = currY + 1;
         }
+        addMoves(currY + 1, X);
     }
 
     public void moveDiagonally(){
-        int X = position.getRow()+1;
-        int Y = position.getColumn()+1;
+        int X = position.getRow();
+        int Y = position.getColumn();
         int currY = Y;
         int currX = X;
-        while(currY < 8){
-            addMoves(currX+1, currY+1);
+        while(currY < 8 && currX < 8){
             currX = currX +1;
             currY = currY +1;
+            if (board.board[currX - 1][currY - 1] != null) {
+                break;
+            }
+            addMoves(currX, currY);
         }
         currX = X;
         currY = Y;
-        while(currY > 1){
-            addMoves(currX-1, currY-1);
+        while(currY > 1 && currX > 1){
             currX = currX -1;
             currY = currY -1;
+            if (board.board[currX-1][currY-1] != null){
+                break;
+            }
+            addMoves(currX, currY);
         }
         currX = X;
         currY = Y;
-        while(currY < 8){
-            addMoves(currX-1, currY+1);
+        while(currY < 8 && currX > 1){
             currX = currX-1;
             currY = currY+1;
+            if (board.board[currX-1][currY-1] != null){
+                break;
+            }
+            addMoves(currX, currY);
         }
         currX = X;
         currY = Y;
-        while(currY > 1){
-            addMoves(currX+1, currY-1);
+        while(currY > 1 && currX < 8){
             currX = currX +1;
             currY = currY-1;
+            if (board.board[currX-1][currY-1] != null){
+                break;
+            }
+            addMoves(currX, currY);
         }
+
     }
 
     public void addMoves(int X, int Y){
