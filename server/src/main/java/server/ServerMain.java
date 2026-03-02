423af4a60f15abb7ea86f2d43801cb9b8e593372
Taylor Christensen <tchris43@byu.edu>
2026-03-01 23:49:26 -0700

    initial commit

--- server/src/main/java/server/ServerMain.java
+++ server/src/main/java/server/ServerMain.java
@@ -6,5 +6,6 @@ public class ServerMain {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+        System.out.println("server running");
     }
 }
