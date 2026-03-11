860a5982ef23ea58cf4715c3cb77b8272e768816
Taylor Christensen <tchris43@byu.edu>
2026-03-10 20:45:48 -0600

    wrote test for createUser

--- server/src/test/java/dataaccess/DatabaseTests.java
+++ server/src/test/java/dataaccess/DatabaseTests.java
@@ -1,6 +1,12 @@
 package dataaccess;
 
+import model.UserData;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class DatabaseTests {
     private DataAccess getDataAccess(Class<? extends DataAccess> databaseClass) throws SQLException, DataAccessException{
@@ -13,4 +19,25 @@ public class DatabaseTests {
         }
         return db;
     }
+
+    @ParameterizedTest
+    @ValueSource(classes = {MySqlDataAccess.class, MemoryDataAccess.class})
+    void createUser(Class<? extends DataAccess> dbClass) throws SQLException, DataAccessException {
+        DataAccess db = getDataAccess(dbClass);
+
+        String username = "taylor";
+        var user = new UserData(username, "password", "taylor@gmail.com");
+        assertDoesNotThrow(() -> db.createUser(username, user));
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {MySqlDataAccess.class, MemoryDataAccess.class})
+    void createUser(Class<? extends DataAccess> dbClass) throws SQLException, DataAccessException {
+        DataAccess db = getDataAccess(dbClass);
+
+        String username = "taylor";
+        var user = new UserData(username, "password", "taylor@gmail.com");
+        assertDoesNotThrow(() -> db.createUser(username, user));
+    }
+
 }
