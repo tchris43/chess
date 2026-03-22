fc56ede3c5eb98ad105731cff2e847b179c41564
Taylor Christensen <tchris43@byu.edu>
2026-03-21 22:19:28 -0600

    first three helpers plus ResponseException for ServerFacade

---
+++ shared/src/main/java/server/ResponseException.java
@@ -0,0 +1,7 @@
+package server;
+
+public class ResponseException extends Exception {
+    public ResponseException(String message) {
+        super(message);
+    }
+}
--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,8 +1,12 @@
 package server;
 
-import model.*
+import com.google.gson.Gson;
+import model.*;
 
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ServerFacade {
     private final HttpClient client = HttpClient.newHttpClient();
@@ -38,6 +42,32 @@ public class ServerFacade {
     public UpdatedGameResult joinGame(JoinRequest joinRequest){
         UpdatedGameResult  updatedGameResult = new UpdatedGameResult("");
         return updatedGameResult;
+    }
+
+    private HttpRequest buildRequest(String method, String path, Object body){
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeRequestBody(body));
+        if (body != null){
+            request.setHeader("Content-Type", "application/json");
+        }
+        return request.build();
+    }
+
+    private HttpRequest.BodyPublisher makeRequestBody(Object request){
+        if (request != null){
+            return HttpRequest.BodyPublishers.ofString(new Gson().toJson(request));
+        } else {
+            return HttpRequest.BodyPublishers.noBody();
+        }
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws ResponseException{
+        try {
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception ex){
+            throw new ResponseException(ex.getMessage());
+        }
     }
 
 
