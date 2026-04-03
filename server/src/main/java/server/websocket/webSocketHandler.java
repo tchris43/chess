bc150c1f36dd4685617789d88e219681872fca67
Taylor Christensen <tchris43@byu.edu>
2026-04-02 21:23:32 -0600

    setup the handleMessage method

--- server/src/main/java/server/websocket/webSocketHandler.java
+++ server/src/main/java/server/websocket/webSocketHandler.java
@@ -1,7 +1,12 @@
 package server.websocket;
 
+import com.google.gson.Gson;
 import io.javalin.websocket.*;
+import jakarta.websocket.Session;
 import org.jetbrains.annotations.NotNull;
+import websocket.commands.UserGameCommand;
+
+import static websocket.commands.UserGameCommand.CommandType.*;
 
 public class webSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
 
@@ -12,12 +17,27 @@ public class webSocketHandler implements WsConnectHandler, WsMessageHandler, WsC
     }
 
     @Override
-    public void handleMessage(@NotNull WsMessageContext wsMessageContext) throws Exception {
-
+    public void handleMessage(WsMessageContext ctx) {
+        try {
+            UserGameCommand command = new Gson().fromJson(ctx.message(), UserGameCommand.class);
+            //TODO: get userName
+            switch (command.getCommandType()){
+                case CONNECT -> connect(userName, ctx.session);
+                case MAKE_MOVE -> makeMove(userName, ctx.session);
+                case LEAVE -> leave(userName, ctx.session);
+                case RESIGN -> resign(userName, ctx.session);
+            }
+        }
     }
 
     @Override
     public void handleClose(@NotNull WsCloseContext wsCloseContext) throws Exception {
-
+        System.out.println("Websocket closed");
     }
+
+    private void connect(String userName, Session session){
+        
+    }
+
+
 }
