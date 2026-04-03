a27f0546839835a134ad208ec61458646a593227
Taylor Christensen <tchris43@byu.edu>
2026-04-02 21:08:45 -0600

    added handleConnect

--- server/src/main/java/server/websocket/webSocketHandler.java
+++ server/src/main/java/server/websocket/webSocketHandler.java
@@ -6,17 +6,18 @@ import org.jetbrains.annotations.NotNull;
 public class webSocketHandler implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
 
     @Override
-    public void handleClose(@NotNull WsCloseContext wsCloseContext) throws Exception {
-
-    }
-
-    @Override
-    public void handleConnect(@NotNull WsConnectContext wsConnectContext) throws Exception {
-
+    public void handleConnect(WsConnectContext ctx{
+        ctx.enableAutomaticPings();
+        System.out.println("Websocket connected");
     }
 
     @Override
     public void handleMessage(@NotNull WsMessageContext wsMessageContext) throws Exception {
+
+    }
+
+    @Override
+    public void handleClose(@NotNull WsCloseContext wsCloseContext) throws Exception {
 
     }
 }
