566b278205895b0d87f2633ecab1df6715a4aabb
Taylor Christensen <tchris43@byu.edu>
2026-04-10 10:21:46 -0600

    WebSocketFacade constructor

--- client/src/main/java/client/websocket/WebSocketFacade.java
+++ client/src/main/java/client/websocket/WebSocketFacade.java
@@ -1,10 +1,37 @@
 package client.websocket;
 
+import com.google.gson.Gson;
 import jakarta.websocket.*;
+import server.ResponseException;
+import websocket.messages.NotificationMessage;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class WebSocketFacade extends Endpoint {
     Session session;
     NotificationHandler notificationHandler;
 
-    public WebSocketFacade(String url, NotificationHandler notificationHandler)
+    public WebSocketFacade(String url, NotificationHandler notificationHandler) throws URISyntaxException, ResponseException {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+            this.notificationHandler = notificationHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message){
+                    NotificationMessage notification = new Gson().fromJson(message, NotificationMessage.class);
+                    notificationHandler.notify(notification);
+                }
+            });
+        } catch (DeploymentException | IOException e) {
+            throw new ResponseException(e.getMessage());
+        } 
+    }
 }
